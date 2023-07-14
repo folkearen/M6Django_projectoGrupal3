78a557b7f83c651b08f02c2da96da987dc8208a9
almubaDev <soy@almuba.dev>
2023-07-13 22:03:42 -0400

    Entrega

--- telovendo/home/views.py
+++ telovendo/home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+import locale
 
 # Create your views here.
 
 def bienvenida(request):
-    return HttpResponse("<h1 style='color:red;'>Bienvenido a te lo vendo</h1>")
+    locale.setlocale(locale.LC_TIME, 'es_ES')
+    time = datetime.datetime.now().strftime('%A %d/%m/%Y %H:%M:%S').capitalize()
+    return render(request, 'home/index.html', {
+        'time' : time
+    } )
 
